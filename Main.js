3b4eac217535acc991eeb36f680e2750f5401245
Takashi@Mac <hamada@gift10.co.jp>
2021-01-24 08:55:39 +0900

    編集用URL

--- Main.js
+++ Main.js
@@ -24,10 +24,13 @@ function doPost (e) {
     var root = DriveApp.getFolderById(ROOT_FOLDER);
     var name = "Vote! : " + getDateString();
     var copy = template.makeCopy(name, root);
+    copy.setSharing(DriveApp.Access.ANYONE, DriveApp.Permission.EDIT);
     // --
+    // メッセージ作成
     var message = lang === "ja" ? "作成しました!" : "Done!";
-    var url = copy.getUrl();
-    sendMessage(message + "\n<" + url + ">");
+    var edit = "[edit]<" + copy.getEditUrl() + ">";
+    var view = "[view]<" + copy.getPublishedUrl() + ">";
+    sendMessage(message + "\n" + edit + "\n" + view);
 }
 
 // 送信部
